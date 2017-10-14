318810f24ebc05bbbe88064c000212b6c315f476
Michael Hewson <michael@michaelhewson.ca>
2017-10-14 01:42:20 -0400

    small tweaks to docs

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,18 @@
 
 use core::ptr;
 
-/// A version of FnOnce that (unsafely) does not take `self` by value.
+/// A version of FnOnce that takes `self` by `&mut` reference instead of by value.
 /// 
-/// This allows you to implement FnOnce on a type owning an FnMove trait object, as long as
-/// the wrapper type is Sized.
+/// This allows you to implement FnOnce on a type owning an FnMove trait object, as long as the wrapper type is Sized.
 /// 
-/// This trait will be obsolete once the [unsized rvalues RFC](https://github.com/rust-lang/rfcs/pull/1909)
-/// is implemented.
+/// This trait will be obsolete once the [unsized rvalues RFC](https://github.com/rust-lang/rfcs/pull/1909) is implemented.
 #[rustc_paren_sugar]
 pub trait FnMove<Args>: FnOnce<Args> {
 
-    /// unsafe because this takes `self` by reference, but treats it as moved.
+    /// # Unsafety
+    /// Unsafe because this takes `self` by reference, but treats it as moved.
     /// The caller must somehow stop the closure's destructor from running afterwards,
-    /// whether that's by calling mem::forget on it or something else.
+    /// whether that's by calling `mem::forget` on it or something else.
     unsafe fn call_move(&mut self, args: Args) -> Self::Output;
 }
 
